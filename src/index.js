ae2b54b247cba12aebe4f2ba930bdf6e22111d10
Aline de Lima Santos <linsantos93@gmail.com>
2018-11-27 15:58:07 -0200

    Alteração na paleta de cores, Navbar e botões

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
+// mudar navbar e seus elementos conforme scroll conforme scroll
+//lembrando que cada "tick" do botão 3 do mouse corresponde a scrollY = 100
+
+window.onscroll = () => {
+    const nav = document.querySelector('.navbar');
+    const logo = document.querySelector(".navbar__li-logo");
+    const menu = document.getElementsByClassName("navbar__li-link");
+
+    if ( window.screen.availHeight <= 700 && this.scrollY >= 600) {
+        nav.className += " navbar__scroll";
+        logo.className += " navbar__li-logo-scroll";
+        changeClassLoop(menu, " navbar__li-link-scroll");
+
+    } else if ( this.scrollY >= 700 ){
+        nav.className += " navbar__scroll";
+        logo.className += " navbar__li-logo-scroll";
+        changeClassLoop(menu, " navbar__li-link-scroll");
+
+    } else {
+        nav.className = "navbar";
+        logo.className = "navbar__li-logo";
+        returnOldClassLoop(menu, "navbar__li-link");
+    };
+};
+
+// função de mudança para array de elementos, no caso, elementos A
+
+function changeClassLoop(element, addedClass) {
+    for ( i = 0; i < element.length; i++ ){
+        element[i].className += addedClass
+    };
+};
+
+function returnOldClassLoop(element, previousClass) {
+    for ( i = 0; i < element.length; i++ ){
+        element[i].className = previousClass
+    };
+};
+
+
 //quando eu clico na div, é para a animação do hamburguer ocorrer
 //e a animação do box shadow e da lista ocorrerem também
 
